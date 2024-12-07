71fca7a8ca05cca19126b9c73fa8e582398b216f
Bruno Oberle <bruno@boberle.com>
2024-12-07 09:32:18 +0100

    add build bash script to zip the extension for AMO

---
+++ build.sh
@@ -0,0 +1,11 @@
+zip -r vinted_downloader.zip \
+  manifest.json \
+  content_scripts/download.js \
+  icons/icon16.png \
+  icons/icon32.png \
+  icons/icon48.png \
+  icons/icon64.png \
+  icons/icon96.png \
+  popup/popup.css \
+  popup/popup.html \
+  popup/popup.js
